5e5ac42790580d659f8b2728d78f1fb646af8af5
ChenNing02 <2633565885@qq.com>
2021-06-15 12:27:38 +0800

    测试同时支持全局/按需引入两种方式

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import 'tulip-ui/dist/css/main.css'
-import tulip from 'tulip-ui'
-const { Button, ButtonGroup } = tulip
-console.log('Button')
-console.log(Button)
+// import tulip from 'tulip-ui'
+import { Button, ButtonGroup } from 'tulip-ui'
 
 Vue.config.productionTip = false
+
+// console.log('tulip')
+// console.log(tulip)
+
+// Vue.use(tulip)
 
 Vue.use(Button)
 Vue.use(ButtonGroup)
